0dc5f918b123c72b197d9ad38f9c290dc3dff06c
LolHAHAHAHALOL <101388074+LolHAHAHAHALOL@users.noreply.github.com>
2023-07-31 19:58:58 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,107 @@
+function checkfordisp() {
+    sf = document.getElementById("searchbar");
+    sbtn = document.getElementById("sbtn");
+    console.log(sf);
+    console.log(sbtn);
+
+    currentstyle = window
+      .getComputedStyle(document.getElementById("searchbar"))
+      .getPropertyValue("display");
+    homestyle = window
+      .getComputedStyle(document.getElementById("homesec"))
+      .getPropertyValue("display");
+    aboutstyle = window
+      .getComputedStyle(document.getElementById("aboutsec"))
+      .getPropertyValue("display");
+    faqstyle = window
+      .getComputedStyle(document.getElementById("faqsec"))
+      .getPropertyValue("display");
+
+    if (currentstyle == "none") {
+      sf.style.display = "block";
+      sbtn.style.display = "block";
+    } else {
+      sf.style.display = "none";
+      sbtn.style.display = "none";
+    }
+  }
+
+  function togglemenuoff() {
+    menupos = window
+      .getComputedStyle(document.getElementById("menu"))
+      .getPropertyValue("left");
+    menu = document.getElementById("menu");
+    mbtn = document.getElementById("togmenu");
+    menu.style.left = "-10000px";
+    mbtn.setAttribute("onclick", "togglemenuon()");
+  }
+  function togglemenuon() {
+    menupos = window
+      .getComputedStyle(document.getElementById("menu"))
+      .getPropertyValue("left");
+    menu = document.getElementById("menu");
+    mbtn = document.getElementById("togmenu");
+    menu.style.left = "0px";
+    mbtn.setAttribute("onclick", "togglemenuoff()");
+  }
+
+  const faqs = document.querySelectorAll(".faq");
+
+  faqs.forEach((faq) => {
+    const question = faq.querySelector(".question");
+    const answer = faq.querySelector(".answer");
+
+    question.addEventListener("click", () => {
+      faq.classList.toggle("active");
+      const isActive = faq.classList.contains("active");
+
+      if (isActive) {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        question.querySelector("svg").style.transform = "rotate(180deg)";
+      } else {
+        answer.style.maxHeight = "0";
+        question.querySelector("svg").style.transform = "rotate(0deg)";
+      }
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+const userInput = document.getElementById("searchbar");
+const searchButton = document.getElementById("sbtn"); // Get the search button with id "sbtn"
+
+userInput.addEventListener("keydown", function (event) {
+if (event.key === "Enter") {
+event.preventDefault();
+Searcher();
+}
+});
+
+searchButton.addEventListener("click", function () {
+Searcher();
+});
+
+function Searcher() {
+const query = userInput.value;
+const searchRegex = new RegExp(query, "gi");
+const elementsWithQuery = document.querySelectorAll("*");
+
+elementsWithQuery.forEach((element) => {
+const textContent = element.textContent;
+if (searchRegex.test(textContent)) {
+togglemenuoff();
+element.scrollIntoView({ behavior: "smooth", block: "start" });
+userInput.style.backgroundColor = "green";
+setTimeout(() => {
+  userInput.style.backgroundColor = "white";
+}, 500);
+return;
+} else {
+userInput.style.backgroundColor = "red";
+setTimeout(() => {
+  userInput.style.backgroundColor = "white";
+}, 500);
+return false;
+}
+});
+}
+});
